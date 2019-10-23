f85e9d51ac989312456e78d1b702cac6fbcaaf30
taliabahar <taliabahar@yahoo.com>
2019-10-23 10:40:27 -0700

    Add files via upload

---
+++ src/TrackingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TrackingPage() {
+
+    return(
+        <div className="TrackingPage">
+        <h1>Tracking Page</h1>
+        </div>
+    )
+}
+
+export default TrackingPage;
